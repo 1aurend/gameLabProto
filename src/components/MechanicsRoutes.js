a4d33cae440e0e99feefeb4323368635caac700a
Lauren Davidson <laurendavidson@Laurens-MacBook-Pro.local>
2019-04-22 14:37:15 -0400

    fix MRs

---
+++ src/components/MechanicsRoutes.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import{ Link, Switch, Route } from 'react-router-dom';
+import './components.css';
+import MechanicsGrid from './MechanicsGrid';
+import mechanicsList from '../mechanicslist.js'
+import Moves from './Moves.js'
+import Page from './MovePage.js'
+import Mechanic from './Mechanic.js'
+
+
+
+function MechanicsRoutes(props) {
+
+  console.log(JSON.stringify(props));
+  const level = props.location.pathname.split("/");
+  console.log(level);
+  let nestedPath;
+
+  if (level.length === 4) {
+    nestedPath = <Route path={props.location.pathname} component={Page}/>
+  }
+  else {
+    nestedPath = <Route path={props.location.pathname} component={Mechanic}/>
+  }
+
+    return (
+      <Switch>
+        <Route exact path='/mechanics' component={MechanicsGrid}/>
+        {nestedPath}
+      </Switch>
+    );
+
+
+}
+
+
+export default MechanicsRoutes;
