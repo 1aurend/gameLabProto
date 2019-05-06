afc5fb43484ec7af77aa76b4c89827eb163851c5
philsometimes <philsometimes@gmail.com>
2019-05-06 15:23:19 -0400

    changed demo

--- src/components/subpages/Tetrad.js
+++ src/components/subpages/Tetrad.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../grids.css'
-import tetrad from '../../assets/color-tetrad-new.png'
+import tetradpng from '../../assets/color-tetrad-new.png'
 import DiceNav from '../dice_nav/DiceNav.js'
 import '../subpages.css'
 import gameLab from '../../assets/dev_subtitle.svg'
 import Collapsible from 'react-collapsible'
+import { Link } from 'react-router-dom'
+import playtest from '../../assets/playtest.svg'
+import tetrad from '../../assets/tetrad.svg'
+import designchallenge from '../../assets/designchallenge.svg'
+
 
 function Tetrad (props) {
 
@@ -18,25 +23,38 @@ function Tetrad (props) {
           <DiceNav path={props.location.pathname}/>
         </div>
       </div>
-      
-        <div id='seminarBlurb'>
-          <p>Schell’s tetrad is one way of breaking games down into components and analyzing the interactions between them. Analyzing your own classroom in this way is a great first step for thinking like a game designer. You can use the tetrad to analyze your course at the level of an entire syllabus, a single course meeting or unit, or something as small as a single assignment. Each level will yield distinct insights.
-          </p>
-          <Collapsible trigger="How do I start?" className='Collapsible__trigger'>
-            <p>This is the collapsible content. It can be any element or React component you like.</p>
-          </Collapsible>
-          <Collapsible trigger="I know what Mechanics are, but what do you mean by the others?" className='Collapsible__trigger'>
-            <p>This is the collapsible content. It can be any element or React component you like.</p>
-          </Collapsible>
-          <Collapsible trigger="I've filled out the tetrad for my class. Now what?" className='Collapsible__trigger'>
-            <p>This is the collapsible content. It can be any element or React component you like.</p>
-          </Collapsible>
+      <div id='toolsNav'>
+        <div id='playtestrow'>
+          <Link className='imgLink' to='/tools/playtest'><img src={ playtest } /></Link>
+          <Link className='pLink' to='/tools/playtest'><h4>“Playtest”<br />Your Assignment</h4></Link>
         </div>
-        <div id='tetradpng'>
-          <img src={tetrad} alt='tetrad' style={{width: '100%'}}/>
-
+        <div id='tetradrow'>
+          <Link className='imgLink' to='/tools/tetrad'><img src={ tetrad } /></Link>
+          <Link className='pLink' to='/tools/tetrad'><h4>The<br />Tetrad</h4></Link>
+        </div>
+        <div id='designchallengerow'>
+          <Link className='imgLink' to='/tools/challenges'><img src={ designchallenge } /></Link>
+          <Link className='pLink' to='/tools/challenges'><h4>Design<br />Challenges</h4></Link>
         </div>
       </div>
+      <div id='seminarBlurb'>
+        <p>Schell’s tetrad is one way of breaking games down into components and analyzing the interactions between them. Analyzing your own classroom in this way is a great first step for thinking like a game designer. You can use the tetrad to analyze your course at the level of an entire syllabus, a single course meeting or unit, or something as small as a single assignment. Each level will yield distinct insights.
+        </p>
+        <Collapsible trigger="How do I start?" className='Collapsible__trigger'>
+          <p>This is the collapsible content. It can be any element or React component you like.</p>
+        </Collapsible>
+        <Collapsible trigger="I know what Mechanics are, but what do you mean by the others?" className='Collapsible__trigger'>
+          <p>This is the collapsible content. It can be any element or React component you like.</p>
+        </Collapsible>
+        <Collapsible trigger="I've filled out the tetrad for my class. Now what?" className='Collapsible__trigger'>
+          <p>This is the collapsible content. It can be any element or React component you like.</p>
+        </Collapsible>
+      </div>
+      <div id='tetradpng'>
+        <img src={tetradpng} alt='tetrad' style={{width: '100%'}}/>
+
+      </div>
+    </div>
 )
 
 }
--- src/components/subpages/ToolsDemo.js
+++ src/components/subpages/ToolsDemo.js
@@ -5,6 +5,11 @@ import DiceNav from '../dice_nav/DiceNav.js'
 import '../subpages.css'
 import gameLab from '../../assets/dev_subtitle.svg'
 import { toolsDemo } from '../homepage/pitchText.js'
+import { Link } from 'react-router-dom'
+import playtest from '../../assets/playtest.svg'
+import tetrad from '../../assets/tetrad.svg'
+import designchallenge from '../../assets/designchallenge.svg'
+
 
 function ToolsDemo (props) {
 
@@ -19,10 +24,26 @@ function ToolsDemo (props) {
           </div>
         </div>
         <div id='toolsBlurb'>
-          {toolsDemo}
-        </div>
+          <p>Ready to try out our toolkit? Here are three ways to get started:</p>
+          <div id='toolsCols'>
+            <div id='playtestcol'>
+              <Link className='imgLink' to='/tools/playtest'><img src={ playtest } /></Link>
+              <Link className='pLink' to='/tools/playtest'><h4>“Playtest”<br />Your Assignment</h4></Link>
+              <p>Use these questions to think like a game designer about an already existing assignment or class activity and up the assignment’s XP.</p>
+            </div>
+            <div id='tetradcol'>
+              <Link className='imgLink' to='/tools/tetrad'><img src={ tetrad } /></Link>
+              <Link className='pLink' to='/tools/tetrad'><h4>The<br />Tetrad</h4></Link>
+              <p>Game designer Jesse Schell uses a tetrad to break down games into their component elements. Not surprisingly, analyzing classes using this tetrad leads to key insights about how they work.</p>
+            </div>
+            <div id='designchallengecol'>
+              <Link className='imgLink' to='/tools/challenges'><img src={ designchallenge } /></Link>
+              <Link className='pLink' to='/tools/challenges'><h4>Design<br />Challenges</h4></Link>
+              <p>Not sure where to begin? Try out some of the design challenges we used in our seminar for crafting game-infused assignments.</p>
+            </div>
+          </div>
       </div>
-
+    </div>
 )
 
 }
